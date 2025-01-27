2524f84dd066fd15bf93cefcf7cbb2a46545b958
Daniel <danieldcn05@gmail.com>
2025-01-27 21:38:46 +0100

    Add UserRepository interface for user data access

---
+++ src/main/java/com/danieldcn05/ticket_backend/ticket_backend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.danieldcn05.ticket_backend.ticket_backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danieldcn05.ticket_backend.ticket_backend.models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+
+}
